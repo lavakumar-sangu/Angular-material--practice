1a0b75b0f0461af68b285db79d6e759009f9802c
lavakumar-sangu <lavakumarsangu@gmail.com>
2021-09-22 18:02:23 +0530

    newfiles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { IntroComponent } from './intro/intro.component';
+import { PaginatorComponent } from './paginator/paginator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginFormComponent },
+  { path: 'loginpage', component: LoginpageComponent },
+  { path: 'home', component: IntroComponent },
+  { path: 'userData', component: PaginatorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/datashare.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+
+@Injectable ({
+    providedIn : "root"
+})
+
+export class datashareService {
+    public content = new BehaviorSubject<string>("");
+    public share = this.content.asObservable();
+  constructor() { }
+  updateData(text:any) {
+    this.content.next(text);
+  }
+}
