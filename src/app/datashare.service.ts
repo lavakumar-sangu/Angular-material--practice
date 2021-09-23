c0ef7c8fe5708b08d8460776466aa6aaec595f57
lavakumar-sangu <lavakumarsangu@gmail.com>
2021-09-23 18:06:28 +0530

    Some changes done

--- src/app/datashare.service.ts
+++ src/app/datashare.service.ts
@@ -1,12 +1,10 @@
  import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+
+ 
 @Injectable ({
 providedIn : "root"
 })
 export class datashareService {
-pop() {
-    throw new Error('Method not implemented.');
-}
 arr:any= [];
 constructor() { }
 setArray(val:any){
--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -16,15 +16,16 @@ styleUrls: ['./paginator.component.css']
 export class PaginatorComponent {
 dataSource = new MatTableDataSource<any>();
 public USER_DATA : user[] =[];
-
 constructor(public dataService :datashareService){}
 ngOnInit(): void {
 this.USER_DATA = this.dataService.getArray();
 this.dataSource.data = this.USER_DATA;
 }
-displayedColumns= ['name', 'passwords','actions'];
-removeData(){
-    this.USER_DATA.pop();
+displayedColumns= ['name', 'passwords', 'actions'];
+removeData(name:number){
+    if (name > -1) {
+    this.USER_DATA.splice(name, 1);
     this.ngOnInit();
+    }
 }
 }
